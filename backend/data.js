f3ede54675c12d3032fcdaaa193b41d71c1c04ff
Saikiran96c <75816166+Saikiran96c@users.noreply.github.com>
2020-12-11 13:34:39 +1300

    Add files via upload

---
+++ backend/data.js
@@ -0,0 +1,76 @@
+const data ={
+    products: [
+        {
+           _id: '1',
+           name: 'BAS',
+           category: 'bats',
+           image: '/images/ss.jpg',
+           price: 120,
+           countInStock: 20,
+           brand: 'BAS',
+           rating: 4.5,
+           reviews: 10,
+           description: 'high quality bats',
+
+        },
+
+        {
+            _id: '2',
+            name: 'ADIDAS',
+            category: 'glows',
+            image: '/images/ss.jpg',
+            price: 140,
+            countInStock: 12,
+            brand:'BAS',
+            rating: 4.1,
+            reviews: 15,
+            description: 'high quality glows',
+ 
+         },
+
+         {
+            _id: '3',
+            name: 'SS',
+            category: 'pads',
+            image: '/images/ss.jpg',
+            price: 90,
+            countInStock: 0,
+            brand: 'BAS',
+            rating: 5,
+            reviews: 17,
+            description: 'high quality pads',
+ 
+         },
+
+         {
+            _id:  '4',
+            name: 'puma',
+            category: 'bats',
+            image: '/images/ss.jpg',
+            price: 190,
+            countInStock: 20,
+            brand: 'BAS',
+            rating: 4.5,
+            reviews: 19,
+            description: 'high quality bats',
+ 
+         },
+
+         {
+            _id: '5',
+            name: 'BAS',
+            category: 'bats',
+            image: '/images/ss.jpg',
+            price: 120,
+            countInStock: 20,
+            brand: 'BAS',
+            rating: 4.5,
+            reviews: 21,
+            description: 'high quality bats',
+ 
+         },
+    ],
+
+};
+
+export default data;
